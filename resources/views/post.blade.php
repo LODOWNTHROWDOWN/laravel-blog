fa7f41f5347723eb8220bc921d79927d1861bc2a
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-08-02 10:43:42 -0400

    Created a route and linked to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. A iaculis at erat pellentesque adipiscing. Odio morbi quis commodo odio aenean sed. Platea
+        dictumst quisque sagittis purus sit amet. Gravida neque convallis a cras. Pulvinar mattis nunc sed blandit
+        libero. Amet venenatis urna cursus eget nunc scelerisque viverra mauris in. Habitasse platea dictumst quisque
+        sagittis purus sit amet volutpat consequat. At risus viverra adipiscing at in. Vulputate sapien nec sagittis
+        aliquam malesuada. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. At auctor urna nunc id
+        cursus metus aliquam eleifend mi.
+    </p>
+</article>
+
+<a href="/">Go Back</a>
+
+</body>
+
