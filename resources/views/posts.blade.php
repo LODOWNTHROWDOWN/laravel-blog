99040c537e9d0cf0f1c5d3826e9c7f80c740963c
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-08-02 12:22:18 -0400

    Added class to Models directory that helps fetch and read the posts within the resources/posts directory. Then looped over that array to display them all on the home page.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,47 +2,11 @@
 <title>My Blog</title>
 <link rel="stylesheet" href="/app.css">
 <body>
-<article>
-    <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. A iaculis at erat pellentesque adipiscing. Odio morbi quis commodo odio aenean sed. Platea
-        dictumst quisque sagittis purus sit amet. Gravida neque convallis a cras. Pulvinar mattis nunc sed blandit
-        libero. Amet venenatis urna cursus eget nunc scelerisque viverra mauris in. Habitasse platea dictumst quisque
-        sagittis purus sit amet volutpat consequat. At risus viverra adipiscing at in. Vulputate sapien nec sagittis
-        aliquam malesuada. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. At auctor urna nunc id
-        cursus metus aliquam eleifend mi.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. A iaculis at erat pellentesque adipiscing. Odio morbi quis commodo odio aenean sed. Platea
-        dictumst quisque sagittis purus sit amet. Gravida neque convallis a cras. Pulvinar mattis nunc sed blandit
-        libero. Amet venenatis urna cursus eget nunc scelerisque viverra mauris in. Habitasse platea dictumst quisque
-        sagittis purus sit amet volutpat consequat. At risus viverra adipiscing at in. Vulputate sapien nec sagittis
-        aliquam malesuada. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. At auctor urna nunc id
-        cursus metus aliquam eleifend mi.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. A iaculis at erat pellentesque adipiscing. Odio morbi quis commodo odio aenean sed. Platea
-        dictumst quisque sagittis purus sit amet. Gravida neque convallis a cras. Pulvinar mattis nunc sed blandit
-        libero. Amet venenatis urna cursus eget nunc scelerisque viverra mauris in. Habitasse platea dictumst quisque
-        sagittis purus sit amet volutpat consequat. At risus viverra adipiscing at in. Vulputate sapien nec sagittis
-        aliquam malesuada. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. At auctor urna nunc id
-        cursus metus aliquam eleifend mi.
-    </p>
-</article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 
 </body>
 
