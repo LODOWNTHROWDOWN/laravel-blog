bdc6f8ed32677b0b6036ffe895fa07b9b775c0ef
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-08-04 17:52:30 -0400

    Extracted dropdown into its own blade components

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
@@ -35,7 +35,7 @@ Route::get('posts/{post:slug}', function (Post $post) {
             'currentCategory' => $category,
             'categories' => Category::all()
         ]);
-    });
+    })->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
