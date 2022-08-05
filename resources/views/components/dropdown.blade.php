3ccc1e2bfbfc3788f8f787c371e64d64e2dc9789
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-08-04 23:07:53 -0400

    Set up search functionality

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -8,7 +8,7 @@
 
 
     {{-- Links --}}
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full rounded-xl mt-2 z-50" style="display: none">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full rounded-xl mt-2 z-50 max-h-52 overflow-auto" style="display: none">
         {{ $slot }}
     </div>
 </div>
