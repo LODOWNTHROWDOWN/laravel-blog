99ba0c8b60bc4069fc0ac0dca3e76af059c3d237
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-08-09 14:03:23 -0400

    Fixed some issues with registration page and handled failed validations + still displaying old inputs after failed validation

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,14 +16,19 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
                            for="username"
-                           >
+                    >
                         Username
                     </label>
 
@@ -31,8 +36,13 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username') }}"
                            required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -46,11 +56,12 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
 
                     @error('email')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
                 </div>
 
@@ -68,6 +79,10 @@
                            id="password"
                            required
                     >
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
