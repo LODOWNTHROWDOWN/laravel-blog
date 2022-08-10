11db7beb39f87946c114eebfb83ab87f4b9db431
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-08-10 17:54:56 -0400

    Created route for page to create a post and made it only accessible by admins

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Services\Newsletter;
+use Exception;
 use Illuminate\Http\Request;
 
 class NewsletterController extends Controller
 {
     public function __invoke(Newsletter $newsletter)
     {
-        request()->validate(['email' => 'required|email']);
+       request()->validate(['email' => 'required|email']);
 
         try {
             $newsletter->subscribe(request('email'));
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             throw Illuminate\Validation\ValidationException::withMessages([
                 'email' => 'This email could not be added to our newsletter list.'
             ]);
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PostCommentsController;
 use \App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-use App\Services\Newsletter;
+use App\Services\MailchimpNewsletter;
 use Illuminate\Support\Facades\Route;
 
 
@@ -24,3 +24,5 @@ Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
