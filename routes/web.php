afc0b78fa2e3762bcd0f6afaeb7ad841efde3f6a
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-08-10 14:27:29 -0400

    Made newsletter form work and cleaned up code

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,21 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use \App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
 
-Route::get('ping', function () {
-    $mailchimp = new \MailchimpMarketing\ApiClient();
 
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us8'
-    ]);
-
-    $response = $mailchimp->lists->addListMember('9b4b1217c5', [
-       'email_address' => 'martinlauren06@gmail.com',
-       'status' => 'subscribed'
-    ]);
-
-    ddd($response);
-});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
